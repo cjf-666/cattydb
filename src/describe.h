81d0eee90b11798abc4b483f8cc77bffc3f61033
chingfantsou <caojingfan@gmail.com>
2013-10-12 07:08:51 +0800

    add describe.h

---
+++ src/describe.h
@@ -0,0 +1,12 @@
+#ifndef _DESCRIBE_H
+#define _DESCRIBE_H
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include "buffer.h"
+
+#define MAXFILENAME 256
+
+void describe_table(char* table_name);
+
