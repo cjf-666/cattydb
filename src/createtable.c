ab472cf9a4185377ab01a9fbffc626ff2cdbba7b
王冀 <ijingobravo@gmail.com>
2013-10-12 02:26:42 +0800

    Update createtable.c

--- src/createtable.c
+++ src/createtable.c
@@ -13,23 +13,25 @@ void creat_table(char* table_name)
         strcat(file_name,".m");
         p = out_buf_create(file_name,"wb");
         for (i = 0; table_name[i]; ++i)
-                buf_push_byte(p, table_name[i]);
-        buf_push_byte(p, '\n');
+                buf_push_char(p, table_name[i]);
+        buf_push_char(p, '\n');
 }
 void creat_col(char* col_name,char* col_type,int num)
 {
         int i;
         for (i = 0; col_name[i]; ++i)
-                buf_push_byte(p, col_name[i]);
-        buf_push_byte(p, ' ');
+                buf_push_char(p, col_name[i]);
+        buf_push_char(p, ' ');
         for (i = 0; col_type[i]; ++i)
-                buf_push_byte(p, col_type[i]);
-        buf_push_byte(p, ' ');
+                buf_push_char(p, col_type[i]);
+        buf_push_char(p, ' ');
         buf_push_int(p, num);
-        buf_push_byte(p, '\n');
+        buf_push_char(p, '\n');
 }
 void creat_table_on_close()
 {
-        buf_push_byte(p, '`');
+        buf_push_char(p, '`');
         out_buf_remove(p);
 }
+
+
