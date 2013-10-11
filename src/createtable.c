1f998e19a4fc2bee9b81a912142779f19d7bb0e0
王冀 <ijingobravo@gmail.com>
2013-10-12 01:37:53 +0800

    Create createtable.c

---
+++ src/createtable.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include "buffer.c"
+
+#define MAXFILENAME 256
+
+tuple_buffer* p;
+void creat_table(char* table_name)
+{
+        int i;
+        char file_name[MAXFILENAME];
+        strcpy(table_name,file_name);
+        strcat(file_name,".m");
+        tuple_buffer* p = out_buf_create(file_name,"wb");
+        for (i = 0; table_name[i]; ++i)
+                buf_push_byte(p, table_name[i]);
+        buf_push_byte(p, '\n');
+}
+void creat_col(char* col_name,char* col_type,int num)
+{
+        int i;
+        for (i = 0; col_name[i]; ++i)
+                buf_push_byte(p, col_name[i]);
+        buf_push_byte(p, ' ');
+        for (i = 0; col_type[i]; ++i)
+                buf_push_byte(p, col_type[i]);
+        buf_push_byte(p, ' ');
+        buf_push_int(p, num);
+        buf_push_byte(p, '\n');
+}
+void creat_table_on_close()
+{
+        buf_push_byte(p, '`');
+        out_buf_remove(p);
+}
+int main()
+{
+
+        return 0;
+}
