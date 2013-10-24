1012f9866d79a4cf24561ee962dccd5e62257dbd
王冀 <ijingobravo@gmail.com>
2013-10-24 23:07:39 +0800

    Update buffer.c

--- src/buffer.c
+++ src/buffer.c
@@ -1,41 +1,134 @@
-#include "table_info.h"
 #include "buffer.h"
 
-void show_table(const char* tb_name)
+typedef struct tuple_buffer {
+    unsigned char buf[BUF_SIZE];
+    unsigned char *top;
+    size_t byte_num;
+    FILE* db_file;
+};
+
+int eobuf(tuple_buffer* tp)
 {
-        table_set(tb_name);
-        char data_name[TABLE_NAME_LEN];
-        strcpy(data_name, tb_name);
-        strcat(data_name, ".dat");
-        tuple_buffer* buf = in_buf_create(data_name);
-        int i;
-        for (i = 0; i < item_num; ++i)
-                printf("%s\t\t\t", item_arr[i].item_name);
-        putchar('\n');
-        while(!eobuf(buf)) {
-                int is_in = !buf_get_char(buf);
-                for (i = 0; i < item_num; ++i) {
-                        if (item_arr[i].item_type[0] == 'I') {
-                                int val = buf_get_int(buf);
-                                if (is_in)
-                                        printf("%d\t\t\t", val);
-                        }
-                        else {
-                                int j;
-                                for (j = 0; j < item_arr[i].item_byte; ++j) {
-                                        char val = buf_get_char(buf);
-                                        if (is_in && val)
-                                                printf("%c", val);
-                                }
-                                if (is_in)
-                                        printf("\t\t\t");
-                        }
-                }
-                putchar('\n');
-        }
-}
-int main()
-{
-        show_table("product");
+    if (feof(tp->db_file) && tp->top == tp->buf + tp->byte_num)
+        return -1;
+    else
         return 0;
+}
+
+tuple_buffer* in_buf_create(char* df_name)
+{
+    tuple_buffer *tp = (tuple_buffer*) malloc(sizeof(tuple_buffer));
+    tp->db_file = fopen(df_name, "rb");
+    tp->byte_num = fread(tp->buf, 1, BUF_SIZE, tp->db_file);
+    tp->top = tp->buf;
+    return tp;
+}
+
+tuple_buffer* out_buf_create(char* df_name, const char* modes)
+{
+    tuple_buffer *tp = (tuple_buffer*) malloc(sizeof(tuple_buffer));
+    tp->db_file = fopen(df_name, modes);
+    tp->top = tp->buf;
+    memset(tp->buf, 0, sizeof(tp->buf));
+    return tp;
+}
+
+tuple_buffer* update_buf_create(char* df_name)
+{
+    tuple_buffer *tp = (tuple_buffer*) malloc(sizeof(tuple_buffer));
+    tp->db_file = fopen(df_name, "r+b");
+    tp->top = tp->buf + BUF_SIZE;
+    tp->byte_num = 0;
+    return tp;
+}
+
+unsigned char* _get_byte(tuple_buffer *tp)
+{
+    if (tp->top == tp->buf + tp->byte_num) {
+        tp->byte_num = fread(tp->buf, 1, BUF_SIZE, tp->db_file);
+        tp->top = tp->buf;
+    }
+    return tp->top++;
+}
+
+char buf_get_char(tuple_buffer *tp)
+{
+    return *(char*)(_get_byte(tp));
+}
+
+int buf_get_int(tuple_buffer *tp)
+{
+    unsigned char *i = _get_byte(tp);
+    _get_byte(tp);
+    _get_byte(tp);
+    _get_byte(tp);
+
+    return *((int*)i);
+}
+
+void _push_byte(tuple_buffer *tp, unsigned char tmp)
+{
+    if (tp->top == BUF_SIZE+tp->buf) {
+        fwrite(tp->buf, 1, BUF_SIZE, tp->db_file);
+        memset(tp->buf, 0, sizeof(tp->buf));
+        tp->top = tp->buf;
+    }
+    *(tp->top++) = tmp;
+
+}
+
+void buf_push_char(tuple_buffer *tp, char tmp)
+{
+    _push_byte(tp,(unsigned char)tmp);
+}
+
+void buf_push_int(tuple_buffer *tp, int tmp)
+{
+    int p = 1;
+    for (p = 0; p < 4; p++)
+        _push_byte(tp, *(((unsigned char*)&tmp)+p));
+}
+
+void _update_byte(tuple_buffer *tp, unsigned char tmp)
+{
+    if (tp->top == tp->buf + tp->byte_num) {
+        fseek(tp->db_file, ftell(tp->db_file) - tp->byte_num, SEEK_SET);
+        fwrite(tp->buf, 1, BUF_SIZE, tp->db_file);
+        tp->byte_num = fread(tp->buf, 1, BUF_SIZE, tp->db_file);
+        tp->top = tp->buf;
+    }
+    *tp -> top++ = tmp;
+}
+
+void buf_update_char(tuple_buffer *tp, char tmp)
+{
+    _update_byte(tp, (unsigned char) tmp);
+}
+
+void buf_update_int(tuple_buffer *tp, int tmp)
+{
+    int p = 1;
+    for (p = 0; p < 4; p++)
+        _update_byte(tp, *(((unsigned char*)&tmp)+p));
+}
+
+void out_buf_remove(tuple_buffer *tp)
+{
+    fwrite(tp->buf, 1, (tp->top)-(tp->buf), tp->db_file);
+    fclose(tp->db_file);
+    free(tp);
+}
+
+void in_buf_remove(tuple_buffer *tp)
+{
+    fclose(tp->db_file);
+    free(tp);
+}
+
+void update_buf_remove(tuple_buffer *tp)
+{
+    fseek(tp->db_file, ftell(tp->db_file) - tp->byte_num, SEEK_SET);
+    fwrite(tp->buf, 1, BUF_SIZE, tp->db_file);
+    fclose(tp->db_file);
+    free(tp);
 }
