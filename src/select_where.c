efe48cdcab636fd29b85f389c982093f663dcd91
王冀 <ijingobravo@gmail.com>
2013-10-25 11:56:00 +0800

    Create select_where.c

---
+++ src/select_where.c
@@ -0,0 +1,57 @@
+#include "select_where.h"
+
+tuple_buffer* dat_file;
+tuple_buffer* new_file;
+
+void select_where_on_open(const char* file, const char* file_new)
+{
+        char tb_m[TABLE_NAME_LEN];
+        char tb_m_s[TABLE_NAME_LEN];
+        char tb_d[TABLE_NAME_LEN];
+        char tb_d_s[TABLE_NAME_LEN];
+        strcpy(tb_m, file);
+        strcat(tb_m, ".m");
+        strcpy(tb_d, file);
+        strcat(tb_d, ".dat");
+
+        strcpy(tb_m_s, file_new);
+        strcat(tb_m_s, ".m");
+        strcpy(tb_d_s, file_new);
+        strcat(tb_d_s, ".dat");
+
+        table_set(file);
+        dat_file = update_buf_create(tb_d);
+        new_file = out_buf_create(tb_d_s, "wb");
+        create_table(file_new);
+        int i;
+        for (i = 0; i < item_num; ++i)
+                create_col(item_arr[i].item_name, item_arr[i].item_type, item_arr[i].item_byte, item_arr[i].is_pr);
+        create_table_on_close();
+}
+void select_where(const char* pro)
+{
+        char item[2][STRING_LENGTH];
+        sscanf(pro, "%s = %s", item[0], item[1]);
+        while (!eobuf(dat_file)) {
+                if (where_equal1(dat_file, item[0], item[1])) {
+                        buf_move_backward(dat_file, get_tot_bytes()+1);
+                        int i;
+                        for (i = 0; i <= get_tot_bytes(); ++i) 
+                                buf_push_char(new_file, buf_get_char(dat_file));
+                }
+        }
+}
+
+void select_where_on_close()
+{
+        out_buf_remove(new_file);
+        update_buf_remove(dat_file);
+}/*
+int main()
+{
+        select_where_on_open("product","tmp");
+        select_where("name = aa");
+        select_where_on_close();
+        show_table("tmp");
+        return 0;
+}*/
